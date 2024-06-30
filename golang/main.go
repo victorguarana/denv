393a18f74f88378b20a963c714bbfeff20ca267c
Victor Guaraná <victorguarana@hotmail.com>
2024-06-30 20:01:05 -0300

    Refactor folders and add new server to nginx

--- golang/main.go
+++ golang/main.go
@@ -4,13 +4,13 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("Golang Response"))
+		_, err := w.Write([]byte("Golang Response\n"))
 		if err != nil {
 			http.Error(w, "Failed to write response", http.StatusInternalServerError)
 		}
 	})
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		panic(err)
 	}
--- makefile
+++
@@ -1,5 +0,0 @@
-build:
-	docker build -t $(IMAGE_NAME) .
-
-run:
-	docker run -p 80:80 $(IMAGE_NAME)
