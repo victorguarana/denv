9831d6e51f1d0924fdd0c3c5de284b902638e264
Victor Guaraná <victorguarana@hotmail.com>
2024-07-02 00:40:27 -0300

    Add rewrite to nginx and change go route

--- golang/main.go
+++ golang/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("Golang Response\n"))
+	http.HandleFunc("/path/{param}", func(w http.ResponseWriter, r *http.Request) {
+		param := r.PathValue("param")
+		response := fmt.Sprintf("Golang Response with param: %s\n", param)
+		_, err := w.Write([]byte(response))
 		if err != nil {
 			http.Error(w, "Failed to write response", http.StatusInternalServerError)
 		}
